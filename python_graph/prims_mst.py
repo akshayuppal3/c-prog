481a250db5deea7846b8b183db97391c0353a4da
Akshay <akshayuppal3@gmail.com>
2018-11-11 23:21:02 -0600

    Updating prims implementation

--- python_graph/prims_mst.py
+++ python_graph/prims_mst.py
@@ -1,9 +1,10 @@
+# implementation of prims algo for MST
 import math
 
-from graph import Graph
+from graph_weighted import Graph_weighted
 
 def prims(graph,start_node):
-    vertice_info = dict()     # Represent each vertice with value and parent
+    vertice_info = dict()     # Represent each vertice : ( value , parent )
     vertices = graph.get_vertices()
     for vertex in vertices:
         vertice_info[vertex] = (math.inf,None)
@@ -11,17 +12,15 @@ def prims(graph,start_node):
     while vertice_info:
         sort = sorted(vertice_info.items(), key=lambda x: x[1])
         min_vertex = sort[0][0]    #signifies the vertex
-        vertice_info.pop(min_vertex)
-        for adj,weight in graph.get_adj(min_vertex):
-        
-
-    return vertice_info
-
+        weight_parent = vertice_info.pop(min_vertex)
+        print(min_vertex, weight_parent)
+        for vertex,weight in graph.get_adj(min_vertex).items():
+            if vertex in vertice_info:
+                vertex_weight= vertice_info[vertex][0]
+                if weight < vertex_weight:
+                    vertice_info[vertex] = (weight,min_vertex)
 
 if __name__ == '__main__':
-    g = Graph()
+    g = Graph_weighted()
     g = g.get_custom_graph()
     info = prims(g,1)
-    print(info)
-    a = sorted(info.items(), key=lambda x: x[1])
-    print(a[0][0])
